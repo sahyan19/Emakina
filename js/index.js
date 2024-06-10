56e3d7e32378c21dba5219ec78810dfd1e6f8690
sahyan <tahirihasinarakotomanga@gmail.com>
2024-06-10 22:29:15 +0300

    index

---
+++ js/index.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll',function(){
+    var  offers_row = document.querySelector('.offers-row');
+    offers_row.classList.add('scroll');
+    var news_col_2 = document.querySelector('news-col-2');
+    news_col_2.classList.add('scroll');
+    var news_col_3 = document.querySelector('news-col-3');
+    news_col_2.classList.add('scroll');
+});
