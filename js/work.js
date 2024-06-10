a047d2622ab69d6fabba87732e05eb9b9a7026e4
sahyan <tahirihasinarakotomanga@gmail.com>
2024-06-10 16:29:50 +0300

    001

---
+++ js/work.js
@@ -0,0 +1,23 @@
+
+let workSpace = document.getElementById("work-card-content");
+
+fetch("../data/work.json")
+    .then((response) => response.json())
+    .then((allData) => {
+        allData.forEach(data => {
+            let container = document.createElement("div");
+            container.className = "work-card-content-col";
+            container.innerHTML = `
+                                <div class="work-card-image"><img src=${data.img_src} alt="" id="work-card-image-size"></div>
+                                <div class="work-card-description">
+                                    <div class="work-card-description-title">${data.title}</div>
+                                    <div class="work-card-description-text">
+                                        ${data.description}
+                                        <div style="margin-top: 1rem;">${data.divDescription}</div>
+                                    </div>
+                                </div>
+                                `;
+            workSpace.appendChild(container);
+        });
+
+    });
